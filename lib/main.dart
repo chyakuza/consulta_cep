b799453bf653ba917bcda99f512d85bbf37970ab
Carlos Henrique <chyakuza@gmail.com>
2020-03-02 21:52:56 -0300

    projeto completo

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'model/cep.dart';
+import 'services/cep_service.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<ConsultaWsCep>.value(
+          value: ConsultaWsCep(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: MyHomePage(title: 'Consulta Cep - Flutter 2019'),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -25,26 +38,107 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-    
+  TextEditingController _controller = TextEditingController();
+  final _resultCep = StreamController<Cep>();
+  Cep cep = Cep();
+
+  @override
+  void initState() {
+    _loadCep();
+    super.initState();
+  }
+
+  _loadCep() async {
+    if (_controller.text.length == 8) {
+      cep = await ConsultaWsCep.getCep(_controller.text);
+
+      _resultCep.sink.add(cep);
+    }
+
+    if (_controller.text.length != 8) {
+      print("Tamanho invalido");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            
+            Form(
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                controller: _controller,
+                decoration:
+                    InputDecoration(labelText: "Digite CEP para consulta."),
+              ),
+            ),
+            cepResultado(),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () => _loadCep(),
         tooltip: 'Consultar CEP',
-        child: Icon(Icons.add),
+        child: Icon(Icons.find_replace),
       ),
     );
+  }
+
+  Widget cepResultado() {
+    return StreamBuilder<Cep>(
+        stream: _resultCep.stream,
+        builder: (context, snapshot) {
+          return snapshot.hasData
+              ? Container(
+                  margin: EdgeInsets.only(top: 20),
+                  height: 120,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      // color: Colors.green[400],
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.1, 1.0),
+                            blurRadius: 5.0),
+                      ]),
+                  child: Card(
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(snapshot.data.logradouro,
+                          style: TextStyle(color: Colors.green,fontSize: 20,fontWeight: FontWeight.bold),),
+
+                          Text(snapshot.data.bairro,
+                          style: TextStyle(color: Colors.green,fontSize: 20,fontWeight: FontWeight.bold),),
+                                                  
+                          Text("${snapshot.data.localidade}  ${snapshot.data.uf}" ,
+                          style: TextStyle(color: Colors.green,fontSize: 20,fontWeight: FontWeight.bold),),
+                          Text("IBGE : ${snapshot.data.ibge}",
+                          style: TextStyle(color: Colors.green,fontSize: 20,fontWeight: FontWeight.bold),),
+                          
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              : Container(
+                  height: 200,
+                  color: Colors.white,
+                  child: Center(
+                    child: Text("Cep não encontrado!"),
+                  ),
+                );
+        });
   }
 }
--- lib/services/cep_service.dart
+++ lib/services/cep_service.dart
@@ -1,9 +1,9 @@
 import 'package:consulta_cep/model/cep.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
-class ConsultaWsCep {
 
-  static Future<Cep> getCep(String cep)async {
+class ConsultaWsCep {
+   static Future<Cep> getCep(String cep)async {
     try {
       String url = "http://viacep.com.br/ws/${cep}/json/";
       print(url);
@@ -11,14 +11,20 @@ class ConsultaWsCep {
       final response = await http.get(url);
       final json = response.body;
       final Map<String, dynamic> mapResponse = convert.json.decode(json);
-
-      if (response.statusCode == 200){
+      print(mapResponse["erro"]);
+      if (mapResponse["erro"] != true){
+        if (response.statusCode == 200){
           Cep cep = Cep.fromJson(mapResponse);
           return cep;
-      }
+        }
+      }  
+      return null;
     } catch (e, exception) {
       print("Erro : $e Excpt> $exception");
     }
-  }
-
+} 
 }
+
+
+
+
